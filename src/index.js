5e62f68a7d1c32b460666910298358a9e7420d54
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-03-12 23:23:54 +0300

    Copipaste code 2

--- src/index.js
+++ src/index.js
@@ -6,3 +6,24 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Values } from 'redux-form-websit-template';
+
+import store from './store';
+import showResults from './showResults'
+import SimpleForm from './components/SimpleForm';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <div style={{  padding: 15  }}>
+            <h2>Simple Form</h2>
+            <SimpleForm onSubmit="showResults"/>
+            <Values form='simple' />
+        </div>
+    </Provider>,
+    document.getElementById('root')
+);
